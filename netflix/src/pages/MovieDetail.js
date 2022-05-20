1ac30a346ebff52ee5603b2dd821c6114c8a3442
Jaewoneeee <93761302+Jaewoneeee@users.noreply.github.com>
2022-05-21 01:09:21 +0900

    CSS : detail page css update

--- netflix/src/pages/MovieDetail.js
+++ netflix/src/pages/MovieDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { movieDetailAction } from '../redux/actions/movieDetailAction'
-import { Badge } from 'react-bootstrap'
+import { Badge, Button } from 'react-bootstrap'
 import { Reviews, RelatedMovies } from '../components'
 import ClipLoader from "react-spinners/ClipLoader";
 
@@ -11,6 +11,7 @@ const MovieDetail = () => {
   let {id} = useParams()
 
   const [change, setChange] = useState(true)
+  const [showMore, setShowMore] = useState(false)
   const dispatch = useDispatch()
   const {detailMovies, movieReviews, relatedMovies, loading} = useSelector(state => state.movies)
 
@@ -23,6 +24,11 @@ const MovieDetail = () => {
   //   return <ClipLoader color='#ffff' loading={loading} size={150} />
   // }
 
+  const changeShowMore = () => {
+    showMore === false ? setShowMore(true) : setShowMore(false)
+  }
+  console.log(showMore)
+
   const changeTrue = () => {
     setChange(true)
   }
@@ -32,33 +38,65 @@ const MovieDetail = () => {
   }
 
   return (
-    <div className='detail'>
-      <div className='detailPoster'
-      style={{backgroundImage :"url("+`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${detailMovies.poster_path}`+")"}}>
-      </div>
-        <div>
-          {
-            detailMovies === ''? false :
-            detailMovies.genres.map((item, index)=>{
-              return <Badge key={index} bg="danger">{item.name}</Badge>
-            })  
-          }
+    <div className='detailPage'>
+      <div className='detailPageTop'>
+
+        <div className='detailPoster'
+        style={{backgroundImage :"url("+`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${detailMovies.poster_path}`+")"}}>
         </div>
-        <h4>{detailMovies.title}</h4>
-        <p>{detailMovies.tagline}</p>
-        <p>{detailMovies.vote_average}</p>
-        <p>{detailMovies.popularity}</p>
-        <p>{detailMovies.adult ? "청불" : "under 18" }</p>
-        <p>{detailMovies.budget}</p>
-        <p>{detailMovies.revenue}</p>
-        <p>{detailMovies.release_date}</p>
-        <p>{detailMovies.runtime}</p>
-        <p>{detailMovies.overview}</p>
-        <button onClick={()=>changeTrue()}>REVIEWS</button>  
-        <button onClick={()=>changeFalse()}>RELATED MOVIES</button>
+
+        <div className='detailInfo'>
+            {
+              detailMovies === ''? false :
+              detailMovies.genres.map((item, index)=>{
+                return <Badge className="detailInfo-1" key={index} bg="danger">{item.name}</Badge>
+              })  
+            }
+        
+          <h1 style={{fontWeight : "bold"}}>{detailMovies.title}</h1>
+          <h5>{detailMovies.tagline}</h5>
+          <div className="detailInfo-2">
+            <h5>★ {detailMovies.vote_average}</h5>
+            <h5>▲ {detailMovies.popularity}</h5>
+            <h5 style={{color : 'red', fontWeight : "bold"}}>{detailMovies.adult ? "청불" : "under18" }</h5>
+          </div>
+          <div className="detailInfo-3">
+            <p>{detailMovies.overview}</p>
+          </div>
+          <div className="detailInfo-3">
+            <div>
+              <Badge bg="danger">Budget</Badge><p>${detailMovies.budget}</p>
+              <Badge bg="danger">Revenue</Badge><p>${detailMovies.revenue}</p>
+            </div>
+            <div>
+              <Badge bg="danger">Release Day</Badge><p>{detailMovies.release_date}</p>
+              <Badge bg="danger">Runtime</Badge><p>{detailMovies.runtime}</p>
+            </div>
+          </div>
+          <div>
+            Watch Trailer
+          </div>
+        </div>
+      </div>
+
+      <div className='detailPageBottom'>
+        <div className='detailPageBottom-top'>
+          <Button onClick={()=>changeShowMore()} variant="danger">Show More</Button>
+        </div>
         {
-          change ? <Reviews review={movieReviews}/> : <RelatedMovies related={relatedMovies}/>
-        } 
+          showMore === false ? null :
+          <div>
+            <div  className='detailPageBottom-middle'>
+              <Button onClick={()=>changeTrue()} variant="danger">REVIEWS</Button>  
+              <Button onClick={()=>changeFalse()} variant="danger">RELATED MOVIES</Button>
+            </div>
+            <div className='detailPageBottom-bottom'>
+              { change ? <Reviews review={movieReviews}/> : <RelatedMovies related={relatedMovies}/>}
+            </div>
+          </div>
+        }
+      </div>
+
     </div>
   )
 }
